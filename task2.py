510a3f09fec1dd78661d841459a3bbd4cf5ec7e4
Max <0665232@mail.ru>
2022-12-25 20:14:43 +0300

    task3 done

--- task2.py
+++ task2.py
@@ -3,14 +3,23 @@
 # - 6782 -> 23
 # - 0,56 -> 11
 
-# a = float(input('Введите число:'))
-# sum = 0
-# while a != 0:
-#     p = a % 10
-#     sum = sum + p
-#     a = a // 10
-# print(sum)              # Не знаю как сделать с целочисленными, нужен разбор. ВОзможно сплитом [a] ?
+# n = input()
+# summ = 0
+# for i in n:
+#     if i.isdigit():
+#         summ+=int(i)
+# print(summ)
 
+# n = float(input())
+# len_num = len(str(n))-1
+# summ=0
+# while n!=int(n):
+#     n= round(n*10,len_num)
+#     print(n)
+# while n>0:
+#     summ+=n%10
+#     n = n//10
+# print(summ)
 
 
 
@@ -21,32 +30,14 @@
 # Пример:
 # - пусть N = 4, тогда [ 1, 2, 6, 24 ] (1, 1*2, 1*2*3, 1*2*3*4)
 
-# def InputNumbers(inputText):
-#     is_OK = False
-#     while not is_OK:
-#         try:
-#             number = int(input(f"{inputText}"))
-#             is_OK = True
-#         except ValueError:
-#             print("Число должно быть integer ")
-#     return number
-
-
-# def mult(n):
-#     if n == 1:
-#         return 1
-#     else:
-#         return n * mult(n - 1)
-
-
-# num = InputNumbers("Введите число: ")
-
-# list = []
-# for e in range(1, num + 1):
-#     list.append(mult(e))
-
-# print(f"Произведения чисел от 1 до {num}:  {list}")           # Код списан и непонят, требуется помощь.
-
+# n = int(input())
+# #пусть N = 4, тогда [ 1, 2, 6, 24 ] (1, 1*2, 1*2*3, 1*2*3*4)
+# fact_list = []
+# factorial = 1
+# for i in range(1,n+1):
+#     factorial*=i
+#     fact_list.append(factorial)
+# print(fact_list)
 
 
 
@@ -56,8 +47,36 @@
 #         Сумма 9.06
 
 
+n = int(input())
+dict_num = {}
+for i in range(1,n+1):
+    dict_num[i] = round((1+1/i)**i,2)
+print(dict_num)
+print(sum(dict_num.values()))
 
-# Нет никакого решения
+
+# 4 задача
+
+
+# import random
+#
+# n = int(input())
+# list_num = [random.randint(-n,n) for i in range(n)]
+# print(list_num)
+
+# file = open("File.txt","r")
+# multi = 1
+# list_str = file.readlines()
+# print(list_str)
+# list_num = list(map(str.strip,list_str))
+# print(list_num)
+# list_num = list(map(int,list_num))
+# print(list_num)
+# for i in file:
+#     multi*=list_num[int(i)]
+# print(multi)
+
+
 
 
 
