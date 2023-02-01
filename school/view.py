7b8e9c8c1fa70d8af46ef0d9bd45f64febfcf0ee
MaximUkolov <114096077+MaximUkolov@users.noreply.github.com>
2023-02-01 21:40:45 +0300

    Delete school directory

--- school/view.py
+++
@@ -1,7 +0,0 @@
-def get_op():
-    op = input("1 - д.у.,\n 2 - д. п.,\n 3 - д. о., \n 4 - показать весь список,\n 5 - показать ученика,\n 6 - выход \n")
-    return op
-
-    def input_student():
-        name = input("Введите имя и фамилию")
-        return name
