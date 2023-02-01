f765a91b734c80ab9d0a927abcb42864af038989
Max <0665232@mail.ru>
2023-02-01 21:39:01 +0300

    Phone book done

---
+++ phonebook/sorting.py
@@ -0,0 +1,27 @@
+def sort_name():
+    with open("worker_list.txt", "r+") as file:
+        lst_with_str = file.readlines()
+        lst_with_lst = []
+        for line in lst_with_str:
+            temp = line.split(",")
+            lst_with_lst.append(temp)
+        lst_with_lst = sorted(lst_with_lst, key = lambda x:x[1])
+        file.sear(0)
+        for worker in lst_with_lst:
+            file.write(",".join(worker))
+        print("Отсортировано")
+        
+
+
+def sort_id():
+    with open("worker_list.txt", "r+") as file:
+        lst_with_str = file.readlines()
+        lst_with_lst = []
+        for line in lst_with_str:
+            temp = line.split(",")
+            lst_with_lst.append(temp)
+        lst_with_lst = sorted(lst_with_lst, key = lambda x:x[0])
+        file.sear(0)
+        for worker in lst_with_lst:
+            file.write(",".join(worker))
+        print("Отсортировано")
---
+++ school/controller.py
@@ -0,0 +1,13 @@
+import view
+
+
+
+
+main_dct = {}
+name_list = {}
+lessons_list = {}
+
+
+def start():
+    op = view.get_op()
+    if op ==1:
